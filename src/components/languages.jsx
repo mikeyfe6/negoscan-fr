b7cf56fde53af2dce9eb180b1f6c8c5164a76a0d
Michael Fransman <mikeyfe6@gimmix.nl>
2020-11-14 01:06:35 +0100

    added single page to front-end, ready for beta client view

--- src/components/languages.jsx
+++ src/components/languages.jsx
@@ -1,57 +1,59 @@
 // import { Link } from "gatsby"
 
 import React from "react"
+import { graphql, Link, useStaticQuery } from "gatsby"
+import Reactmarkdown from "react-markdown"
 
 import languagesStyles from "../styles/modules/languagesStyles.module.scss"
 
 // TODO: count toevoegen
 
-import nodeImg from "../images/node.png"
-import pytImg from "../images/python.png"
-import cshImg from "../images/csharp.png"
-import rubyImg from "../images/ruby.png"
-import phpImg from "../images/php.png"
-import scalaImg from "../images/scala.png"
-import clojImg from "../images/clojure.png"
+import Img from "gatsby-image"
 
-const Languages = () => (
-  <section className={languagesStyles.languages}>
-    <h2
-      className={`${languagesStyles.md} ${languagesStyles.textCenter} ${languagesStyles.my2}`}
-    >
-      Join the club!
-    </h2>
-    <div className={`${languagesStyles.container} ${languagesStyles.flex}`}>
-      <div className={languagesStyles.card}>
-        <h4>Node.js</h4>
-        <img src={nodeImg} alt="" />
+const Languages = () => {
+  const data = useStaticQuery(graphql`
+    query IndexQuery {
+      allStrapiNegosite {
+        edges {
+          node {
+            id
+            profiel
+            biografie
+            avatar {
+              childImageSharp {
+                fixed(width: 200, height: 125) {
+                  ...GatsbyImageSharpFixed
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <section className={languagesStyles.languages}>
+      <h2
+        className={`${languagesStyles.md} ${languagesStyles.textCenter} ${languagesStyles.my2}`}
+      >
+        Join the club!
+      </h2>
+      <div className={`${languagesStyles.container} `}>
+        <ul className={languagesStyles.flex}>
+          {data.allStrapiNegosite.edges.map(document => (
+            <li key={document.node.id} className={languagesStyles.card}>
+              <h2>
+                <Link to={`/${document.node.id}`}>{document.node.profiel}</Link>
+              </h2>
+              <Reactmarkdown source={document.node.biografie} />
+              <Img fixed={document.node.avatar.childImageSharp.fixed} />
+            </li>
+          ))}
+        </ul>{" "}
       </div>
-      <div className={languagesStyles.card}>
-        <h4>Node.js</h4>
-        <img src={pytImg} alt="" />
-      </div>
-      <div className={languagesStyles.card}>
-        <h4>Node.js</h4>
-        <img src={cshImg} alt="" />
-      </div>
-      <div className={languagesStyles.card}>
-        <h4>Node.js</h4>
-        <img src={rubyImg} alt="" />
-      </div>
-      <div className={languagesStyles.card}>
-        <h4>Node.js</h4>
-        <img src={phpImg} alt="" />
-      </div>
-      <div className={languagesStyles.card}>
-        <h4>Node.js</h4>
-        <img src={scalaImg} alt="" />
-      </div>
-      <div className={languagesStyles.card}>
-        <h4>Node.js</h4>
-        <img src={clojImg} alt="" />
-      </div>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
 
 export default Languages
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,6 @@
 import React from "react"
-// import Reactmarkdown from "react-markdown"
-import { graphql } from "gatsby"
-// Link,
-
-// import Img from "gatsby-image"
 
 import Layout from "../components/layout"
-// import Image from "../components/image"
 
 import Showcase from "../components/showcase"
 import Stats from "../components/stats"
@@ -16,52 +10,15 @@ import Languages from "../components/languages"
 
 import SEO from "../components/seo"
 
-const IndexPage = ({ data }) => (
+const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    {/* <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p> */}
     <Showcase />
     <Stats />
     <Cli />
     <Cloud />
     <Languages />
-    {/* <ul>
-      {data.allStrapiNegosite.edges.map(document => (
-        <li key={document.node.id}>
-          <h2>
-            <Link to={`/${document.node.id}`}>{document.node.profiel}</Link>
-          </h2>
-          <Reactmarkdown source={document.node.biografie} />
-          <Img fixed={document.node.avatar.childImageSharp.fixed} />
-        </li>
-      ))}
-    </ul> */}
-    {/*<div> <Image /> </div>*/}
-    {/* <Link to="/admin/login/">Inloggen / Registreren</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
   </Layout>
 )
 
 export default IndexPage
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    allStrapiNegosite {
-      edges {
-        node {
-          id
-          profiel
-          biografie
-          avatar {
-            childImageSharp {
-              fixed(width: 200, height: 125) {
-                ...GatsbyImageSharpFixed
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
